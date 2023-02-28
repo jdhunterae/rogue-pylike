e02373afd54e24ecd471b50d7b6ac9adf8c2dea8
Andrew Pomerleau <arpomerleau@gmail.com>
2023-02-28 16:34:39 -0500

    Added Bump action to check for targets

--- actions.py
+++ actions.py
@@ -49,3 +49,28 @@ class MovementAction(ActionWithDirection):
             return  # Destination is blocked by an entity
 
         entity.move(self.dx, self.dy)
+
+
+class MeleeAction(ActionWithDirection):
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        target = engine.game_map.get_blocking_entity_at_location(
+            dest_x, dest_y)
+
+        if not target:
+            return  # No entity to attack
+
+        print(f"You kick the {target.name}, much to its annoyance!")
+
+
+class BumpAction(ActionWithDirection):
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
+            return MeleeAction(self.dx, self.dy).perform(engine=engine, entity=entity)
+        else:
+            return MovementAction(self.dx, self.dy).perform(engine=engine, entity=entity)
