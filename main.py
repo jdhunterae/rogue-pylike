7066ebcc03f7934dfbbea060a2fcc3709d9e6745
Andrew Pomerleau <arpomerleau@gmail.com>
2023-02-28 14:04:28 -0500

    Added tile types for walls and floor

--- main.py
+++ main.py
@@ -2,13 +2,17 @@
 import tcod
 
 from engine import Engine
-from input_handlers import EventHandler
 from entity import Entity
+from game_map import GameMap
+from input_handlers import EventHandler
 
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
+
+    map_width = 80
+    map_height = 45
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -22,8 +26,10 @@ def main() -> None:
                  int(screen_height / 2), "@", (255, 255, 0))
     entities = {player, npc}
 
-    engine = Engine(entities=entities,
-                    event_handler=event_handler, player=player)
+    game_map = GameMap(map_width, map_height)
+
+    engine = Engine(entities=entities, event_handler=event_handler,
+                    game_map=game_map, player=player)
 
     with tcod.context.new_terminal(
         screen_width,
